d51deaf1717185d85c94e9c50f21a56e08cec29d
root <root@localhost.localdomain>
2017-08-06 19:00:50 +0530

    Test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { NewDirectiveDirective } from './new-directive.directive';
+import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+    NewDirectiveDirective,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule
